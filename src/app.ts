dddfb68074b58ef391ec0712920e27eb7af56ae5
pedroddomingues <pedroddomingues@gmail.com>
2022-01-26 16:37:18 +0000

    testing jwt

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import passport from "passport";
 
-import { mongo_connect } from "./database";
 import "./security/authentication/local";
+import { mongo_connect } from "./database";
 import { log_errors, api_errors } from "./errors";
 import { authentication_router, user_router } from "./routes";
 
@@ -23,6 +23,7 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/auth", authentication_router);
 
+app.use(passport.authenticate("jwt", { session: false }));
 app.use("/users", user_router);
 
 app.use(log_errors);
--- src/security/authentication/jwt.ts
+++ src/security/authentication/jwt.ts
@@ -1,21 +1,25 @@
 import passport from 'passport';
 import { Strategy, ExtractJwt, StrategyOptions } from 'passport-jwt'
 
+import { users_repository } from '../../modules/user.module'
+
 const opts: StrategyOptions = {
 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 	secretOrKey: process.env.JWT_SECRET,
 }
 
 passport.use(new Strategy(opts, function(jwt_payload, done) {
-    User.findOne({id: jwt_payload.sub}, function(err, user) {
-        if (err) {
-            return done(err, false);
-        }
-        if (user) {
-            return done(null, user);
-        } else {
-            return done(null, false);
-            // or you could create a new account
-        }
-    });
+	console.log(jwt_payload);
+	// const user = users_repository.get_by_id(jwt_payload.sub);
+    // User.findOne({id: jwt_payload.sub}, function(err, user) {
+    //     if (err) {
+    //         return done(err, false);
+    //     }
+    //     if (user) {
+    //         return done(null, user);
+    //     } else {
+    //         return done(null, false);
+    //         // or you could create a new account
+    //     }
+    // });
 }));
